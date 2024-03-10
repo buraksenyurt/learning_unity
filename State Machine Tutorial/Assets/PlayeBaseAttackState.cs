76f1d459bc3a3e4000d8eb4895a98baa6486e2c3
buraksenyurt <burakselimsenyurt@gmail.com>
2024-03-10 21:58:55 +0300

    Applied combo attack lesson

--- State Machine Tutorial/Assets/PlayeBaseAttackState.cs
+++ State Machine Tutorial/Assets/PlayeBaseAttackState.cs
@@ -1,5 +1,9 @@
+using UnityEngine;
 public class PlayerBaseAttackState : PlayerState
 {
+    private int comboAttackCounter;
+    private float lastTimeAttacked;
+    private float comboAttackWindow = 2;
     public PlayerBaseAttackState(Player player, PlayerStateMachine playerStateMachine, string animBoolName)
         : base(player, playerStateMachine, animBoolName)
     {
@@ -8,6 +12,10 @@ public class PlayerBaseAttackState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        if (comboAttackCounter > 2 || Time.time >= lastTimeAttacked + comboAttackWindow)
+            comboAttackCounter = 0;
+
+        _player.Anim.SetInteger("ComboAttackCounter", comboAttackCounter);
     }
 
     public override void Update()
@@ -22,5 +30,7 @@ public class PlayerBaseAttackState : PlayerState
     public override void Exit()
     {
         base.Exit();
+        comboAttackCounter++;
+        lastTimeAttacked = Time.time;
     }
 }
